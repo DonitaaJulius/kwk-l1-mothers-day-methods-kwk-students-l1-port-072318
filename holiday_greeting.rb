851b2b1e79fb10746aaf0f5f2886101829371c56
DonitaaJulius <donitaajulius@gmail.com>
2018-07-24 18:15:07 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,13 +1,4 @@
 def holiday(to,from,event)
-  puts "Who is the card to?"
-  to = gets.strip
-  puts "This is card for #{to}"
-  puts "Who is the card from?"
-  from = gets.strip
-  puts "This is card for #{from}"
-  puts "When is the card given?"
-  event = gets.strip
-  puts "This card is given on #{event}"
   puts "Happy " + event + ", " + to + "!" + "From " + from
 end
 
